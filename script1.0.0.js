c20e9feb22d27087703dd21d2fd32b9e9f78b98b
Dinkeres <121654391+Dinkeres@users.noreply.github.com>
2024-10-14 18:03:12 -0400

    Update and rename script1.0.1.js to script1.0.0.js

--- script1.0.0.js
+++ script1.0.0.js
@@ -1,7 +1,7 @@
 const grid = document.querySelector('.grid');
 const numColumns = 5;
 const numButtonsPerColumn = 50; // generate 50 buttons per column
-const columnNames = ["GBA", "3DS", "test2", "hi", "test"];
+const columnNames = ["GBA", "3DS", "NDS", "PSX", "ATARIA"];
 const buttonNames = [
     [
         "Button 1", "Button 2", "Button 3", "Button 4", "Button 5",
